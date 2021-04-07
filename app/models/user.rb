57099e7ca14f5f15899563c57b87676f9eccb0bc
massive2351 <ec2-user@ip-172-31-6-225.ec2.internal>
2021-04-07 07:46:10 +0000

    コメント機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
   has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
  root to:'homes#top'
  
- resources :post_images, only: [:new, :show, :index, :create, :destroy]
+ resources :post_images, only: [:new, :show, :index, :create, :destroy] do
+   resources :post_comments, only: [:create, :destroy]
+ end
 end
