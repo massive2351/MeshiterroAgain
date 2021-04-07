ec50d988f3d130716ae964b2c3477335865747e2
massive2351 <ec2-user@ip-172-31-6-225.ec2.internal>
2021-04-07 07:01:26 +0000

    画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6ac23e1d0c2863b3dc63f17c8d541b2cd5931fe72f5b06557c7df60d2fba32f54e8e3495b15b2258d192ab1b1a56353734ca8b85eb7914ce3acea43911a1769e'
